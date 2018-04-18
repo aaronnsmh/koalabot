7f76192486685ee6cd310004089b4e6293f1aa93
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-18 21:12:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,8 +11,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', () => {
-    console.log('RoGiveaway is ONLINE. No bugs found at this time.');
-    client.user.setAvatar('https://lh3.googleusercontent.com/Zqr4Fjvv96ViJHj72zhDv36RWxe1myScn-g2Qx6P-2nGMVyp0PVDMK2kKF0DQz_59t2mR8hf6UbzLQ=w1920-h943')
+    console.log('Koala Bot is ONLINE. No bugs found at this time.');
+    client.user.setAvatar('https://t0.rbxcdn.com/ab0108dde98f01187a1ceed0f4c878a0')
 });
 
 client.on('message', message => {
@@ -21,16 +21,7 @@ client.on('message', message => {
   	}
 });
 
-client.on('message', message => {
-    if (message.content === 'message') {
-        var guildList = client.guilds.array();
-        try {
-            guildList.forEach(guild => guild.defaultChannel.send("messageToSend"));
-        } catch (err) {
-            console.log("Could not send message to ");
-        }
-    }
-});
+
 
 client.on('message', message => {    
     
@@ -38,7 +29,7 @@ client.on('message', message => {
 
     if (sender.bot) return;
     if (message.channel.type === 'dm') {
-        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: https://discord.gg/MKHXv2E) to use the bot! **")
+        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: https://discord.gg/FtYeKMu) to use the bot! **")
         return;
     }
 
@@ -66,12 +57,10 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
     var embedinfo = new Discord.RichEmbed()
-        .setFooter("© RoGiveaway Bot | Made by Aaron #1742")
+        .setFooter("© Koala Bot | Made by Aaron #1742")
         .setColor(randomColor)
-        .setTitle("📖 RoGiveaway Information")
-        .setDescription("Welcome to the RoGiveaway server! Reflictance and Tinsel_Tay are pleased to have you join our server! Be prepared to be amazed with many more giveaways.")
-        .addField("What is RoGiveaway?","RoGiveaway is a leader in robux and account giveaways on Roblox. With hundreds of members already enjoying the service we provide!")
-        .addField("How do I win?","It's easy! Just check the giveaways channel for giveaways, when one is hosted, all you need to do is react to the message with a :tada:. Once a giveaway has finished, if you won the giveaway bot will ping you, simply DM the host to claim your prize! It's that easy")
+        .setTitle(":wave: Welcome")
+        .setDescription("Welcome to the Koala Cafe server!")
      member.sendEmbed(embedinfo);
        console.log(`${member.user.username} has joined`);
 });
