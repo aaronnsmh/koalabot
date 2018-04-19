7ad84a924fdce0f6ce8e5deec88a6c48bc7756f3
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:24:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = 'KC';
 const db = require('quick.db');
-const clbot = new Cleverbot;
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 
@@ -36,14 +35,8 @@ client.on('message', message => {
     
     if (sender.bot) return;
     if (message.channel.type === 'dm') {
-         clbot.write(message.content, (response) => {
-      message.channel.startTyping();
-      setTimeout(() => {
-        message.channel.send(response.output).catch(console.error);
-        message.channel.stopTyping();
-      }, Math.random() * (1 - 3) + 1 * 1000);
-    });
-  }
+        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: https://discord.gg/FtYeKMu) to use the bot! **")
+        return;
     }
     
     if (message.mentions.members.firstKey() === '422393536420511752') {
