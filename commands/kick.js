575e32b2a1ccb95cb33c5f26d8665441e5193a07
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:52:05 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,20 +6,10 @@ exports.run = (Discord, client, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
     if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
-    let kickEmbed = new Discord.RichEmbed()
-        .setDescription("~Kick~")
-        .setColor("#e56b00")
-        .addField("Kicked User", `${kUser} with ID: ${kUser.id}`)
-        .addField("Kicked By", `<@${message.author}> with ID: ${message.author.id}`)
-        .addField("Kicked In", message.channel)
-        .addField("Tiime", message.createdAt)
-        .addField("Reason", kReason);
 
-    message.guild.member(kUser).kick(kReason);
+    message.guild.member(kUser).ban(kReason);
     let kickChannel = '419094345858154496';
     let pkickchannel = message.guild.channels.find(`name`, "logs");
+    message.channel.send('User banned!')
     if (!pkickChannel) return message.channel.send("I can not find the log channel, the user has been kicked. For logs, get the sever admin to make a channel called `logs`.");
 
-    kickChannel.send(kickEmbed);
-    pkickchannel.send(kickEmbed)
-}
