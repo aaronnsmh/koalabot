b13043a5d926f07941bc040db368354f04bee1e6
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 17:18:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '🐨';
+const prefix = 'KC';
 const db = require('quick.db');
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
