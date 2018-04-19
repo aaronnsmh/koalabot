02afc986b96a0cbffd71921eccded509f61316bf
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 17:02:55 +0100

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -2,14 +2,14 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 exports.run = (Discord, client, message, args) => {
 
-    message.reply("For cmds, please check your DMs!");
+    message.reply("Commands:");
     var embedcmds= new Discord.RichEmbed()
         .setFooter("© Koala Bot | Made by Aaron (Arron)")
         .setColor(randomColor)
         .setTitle(":book: Koala Cafe Commands :koala: ")
         .setDescription("Wondering what I do? Well you sir/ma'am have a very good question! Read on to see why I am here!")
         .addField(":clock: More CMDS coming soon", "What do you expect me to put here?")
-    message.author.sendEmbed(embedcmds);
+    message.channel.send.sendEmbed(embedcmds);
     message.delete(10000)
 
 }
