a8c42726029baf6db41bb635049d94ee833c00db
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:33:18 +0100

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -10,6 +10,7 @@ exports.run = (Discord, client, message, args) => {
         .setDescription("Wondering what I do? Well you sir/ma'am have a very good question! Read on to see why I am here!")
         .addField(":8ball: 8Ball [QUESTION]", "Responds with what will happen.")
         .addField(":o: cointoss", "Tosses a coin.")
+        .addField(":frame_photo: avatar", "Shows a picture of your bootiful avatar!")
         .addField(":loudspeaker: say [STRING]", "The bot says what you ask it to say, well duh.")
         .addField(":clock1: More CMDS coming soon", "What do you expect me to put here?")
     message.channel.sendEmbed(embedcmds);
