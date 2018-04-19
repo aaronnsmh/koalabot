8dde7cf7a4a5230d857ae787dbda09cad258df15
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 17:15:24 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,5 @@
 exports.run = (Discord, client, message, args) => {
-const delay = require('delay');
+
 let sender = message.author;
 let allowedRole = message.guild.roles.find("name", "bot admin");
 
@@ -9,10 +9,6 @@ let allowedRole = message.guild.roles.find("name", "bot admin");
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
-    delay(200)
-    .then(() => {
-        message.delete().catch(O_o=>{});
-    });
         
        } else {
          message.channel.send("This command is limited to bot admins only.")
