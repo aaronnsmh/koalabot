a522a754ff0de9771074cba19098f3debbe75071
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:41:24 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,25 +1,25 @@
 exports.run = (Discord, client, message, args) => {
 
-if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
+    let kickEmbed = new Discord.RichEmbed()
+        .setDescription("~Kick~")
+        .setColor("#e56b00")
+        .addField("Kicked User", `${kUser} with ID: ${kUser.id}`)
+        .addField("Kicked By", `<@${message.author}> with ID: ${message.author.id}`)
+        .addField("Kicked In", message.channel)
+        .addField("Tiime", message.createdAt)
+        .addField("Reason", kReason);
+
+    message.guild.member(kUser).kick(kReason);
+    let kickChannel = '419094345858154496';
+    let pkickchannel = message.guild.channels.find(`name`, "logs");
+    if (!pkickChannel) return message.channel.send("I can not find the log channel, the user has been kicked. For logs, get the sever admin to make a channel called `logs`.");
+
+    kickChannel.send(kickEmbed);
+    pkickchannel.send(kickEmbed)
 }
