05493e247dc15768c5df82d37f3409b61e51cc67
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 19:54:18 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,10 +1,5 @@
+
 exports.run = (Discord, client, message, args) => {
-var embederrordm = new Discord.RichEmbed()
-        .setTitle('${message.author.username}s avatar:')
-        .setImage(${message.author.avatar})
-        .setFooter('Koala Bot | Made by Aaron')
-        .setColor('#000000');
-message.reply('Here is your avatar:');
-message.channel.send(${message.author.avatar});
-message.channel.sendEmbed(embederrordm);
+
+message.reply(message.author.avatarURL);
 }
