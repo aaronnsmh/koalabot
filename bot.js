777b4336251f35d6de05cd32f96de63b6cdfcf6b
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 16:10:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,9 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setActivity('over Koala Cafe | !help', { type: 'WATCHING' });
-    client.user.setStatus("online");
+    client.user.setPresence({ activity: { name: 'at Koala Cafe!' }, status: 'idle' })
+  .then(console.log)
+  .catch(console.error);
 });
 
 client.on('ready', () => {
