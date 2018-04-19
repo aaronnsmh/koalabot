8f39f33fd9919745f7b3a08860972f2c89c39744
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:28:53 +0100

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -8,6 +8,9 @@ exports.run = (Discord, client, message, args) => {
         .setColor(randomColor)
         .setTitle(":book: Koala Cafe Commands :koala: ")
         .setDescription("Wondering what I do? Well you sir/ma'am have a very good question! Read on to see why I am here!")
+        .addField(":8ball: 8Ball [QUESTION]", "Responds with what will happen.")
+        .addField(":o:cointoss", "Tosses a coin.")
+        .addField(":loudspeaker: say [STRING]", "The bot says what you ask it to say, well duh.")
         .addField(":clock: More CMDS coming soon", "What do you expect me to put here?")
     message.channel.sendEmbed(embedcmds);
     message.delete(10000)
