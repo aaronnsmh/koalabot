c361f537a91f90fa30637b452ad0a6e1b6d6135f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-19 18:54:29 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -7,9 +7,11 @@ exports.run = (Discord, client, message, args) => {
     if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
 
-    message.guild.member(kUser).ban(kReason);
+    message.guild.member(kUser).kcik(kReason);
     let kickChannel = '419094345858154496';
     let pkickchannel = message.guild.channels.find(`name`, "logs");
-    message.channel.send('User banned!')
+    message.channel.send('User kicked!')
     if (!pkickChannel) return message.channel.send("I can not find the log channel, the user has been kicked. For logs, get the sever admin to make a channel called `logs`.");
 
+    
+}
