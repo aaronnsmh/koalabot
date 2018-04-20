a72d011bbc0b60ce0bf3f8bac8c85b52a247031d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 07:37:49 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,7 @@
 exports.run = (Discord, client, message, args) => {
 
 function doMagic8BallVoodoo() {
-    var rand = [':8ball: Absolutely.', ':8ball: Absolutely not.', ':8ball: It is true.', ':8ball: Impossible.', ':8ball: Of course.', ':8ball: I do not think so.', ':8ball: It is true.', ':8ball: It is not true.', ':8ball: I am very undoubtful of that.', ':8ball: I am very doubtful of that.', ':8ball: Sources point to no.', ':8ball: Theories prove it.', ':8ball: Reply hazy try again', ':8ball: Ask again later', ':8ball: Better not tell you now', ':8ball: Cannot predict now', ':8ball: Concentrate and ask again'];
+    var rand = [':8ball: It is certain',':8ball: As I see it, yes',':8ball: It is decidedly so',':8ball: Without a doubt',':8ball: Yes definitely',':8ball: You may rely on it',':8ball: Most Likely',':8ball: Outlook good',':8ball: Yes',':8ball: Signs point to yes',':8ball: Reply hazy try again',':8ball: Ask again later',':8ball: Better not tell you now',':8ball: Cannot predict now',':8ball: Concentrate and ask again',':8ball: Do not count on it',':8ball: My reply is no',':8ball: My sources say no',':8ball: Outlook not so good',':8ball: Very doubtful'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
