456369b5cc249f6065acaa49cd57a5b80c6c6c83
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 20:09:27 +0100

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,15 @@
+exports.run = (Discord, client, message, args) => {
+
+if (message.member.hasPermission("ADMINISTRATOR")) {
+		   const color = args[0]
+				
+		   const text = args.slice(1).join(" ");
+		   if (text.length < 1) return message.channel.send("Can not announce nothing");
+		   //const colour = args.slice(2).join("");
+		   const embed = new Discord.RichEmbed()
+		   .setColor(#00ff00")
+		   .setTitle("Announcement:")
+		   .setDescription(text);
+		   message.channel.send({embed})
+	   }
+}
