6409c5ce154ab3aea3f36bcbd9368f208ced8459
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 19:22:04 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -3,7 +3,5 @@ try {
 			get('https://aws.random.cat/meow').then(res => {
 				return message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
 			});
-		} catch(err) {
-			return message.channel.send(error.stack);
-		}
+		} 
 }
