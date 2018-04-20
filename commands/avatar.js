3f7101a2b915d0392a0905cb4d7b5a671504be38
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 15:51:32 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 exports.run = (Discord, client, message, args) => {
 if(message.mentions.users.first()) { //Check if the message has a mention in it.
           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
-          let output = ":koala: Avatar URL: " + user.avatarURL + "Isn't it so bootiful!";
+          let output = ":koala: Avatar URL: " + user.avatarURL + " it so bootiful!";
     
           message.channel.sendMessage(output); //We send the output in the current channel.
         
