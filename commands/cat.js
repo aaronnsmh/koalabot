2f614bbd7d0a033ab766b1f1e74bafc942105918
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 19:19:39 +0100

    Create cat.js

---
+++ commands/cat.js
@@ -0,0 +1,9 @@
+exports.run = (Discord, client, message, args) => {
+try {
+			get('https://aws.random.cat/meow').then(res => {
+				return message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
+			});
+		} catch(err) {
+			return msg.channel.send(error.stack);
+		}
+}
