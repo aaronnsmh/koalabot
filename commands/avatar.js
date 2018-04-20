5bd2440901b3399717549b0560b4b9adf05f95f7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 15:42:20 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,10 +1,21 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 exports.run = (Discord, client, message, args) => {
 if(message.mentions.users.first()) { //Check if the message has a mention in it.
           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
           let output = user.username + user.discriminator /*Username and Discriminator*/ +
+          let userpic = user.avatarURL;
+          let username = user.username;
           "\nAvatar URL: " + user.avatarURL; /*The Avatar URL*/
-          message.channel.sendMessage(output); //We send the output in the current channel.
+          /*message.channel.sendMessage(output); //We send the output in the current channel.*/
+          
+          var embedcmds= new Discord.RichEmbed()
+        .setFooter("© Koala Bot | Made by Aaron (Arron)")
+        .setColor(randomColor)
+        .setTitle(username)
+        .setImage(userurl)
+          message.channel.sendEmbed(embedcmds);
+          
     } else {
           message.reply("Invalid user."); //Reply with a mention saying "Invalid user."
     }
