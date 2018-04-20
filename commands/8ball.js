52aa0754eb12f3c2939c2ddafb565ed8a92b5220
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 21:12:10 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,7 +5,15 @@ function doMagic8BallVoodoo() {
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
-
-    message.channel.sendMessage(doMagic8BallVoodoo())
+     const author = message.author;
+		   const embed = new Discord.RichEmbed()
+		   .setColor("#FF0000")
+		   .setThumbnail("https://pbs.twimg.com/profile_images/178167256/8ball.jpg.jpeg")
+		   .setTitle("Magic 8 ball :8ball:")
+		   .setDescription(doMagic8BallVoodoo())
+		   .setFooter("Requested by " + author + " at ")
+		   .setTimestamp()
+		   message.channel.send(":8ball from: " + author);
+		   message.channel.send({embed})
     
 }
