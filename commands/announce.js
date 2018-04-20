4fd34ead477d47e69cd384349fa94ad31f9de3ae
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-20 20:14:29 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -4,11 +4,13 @@ if (message.member.hasPermission("ADMINISTRATOR")) {
 		   const color = args[0]
 				
 		   const text = args.slice(1).join(" ");
+		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-		   .setTitle("Announcement:")
+		   .setAuthor(author)
+		   .setTitle(":mega: Announcement:")
 		   .setDescription(text);
 		   message.channel.send({embed})
 	   }
