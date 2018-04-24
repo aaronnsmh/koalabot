1f63644571e8660e5ca01519a1957091c12335cb
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 13:11:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,63 +2,60 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = 'KC';
 const db = require('quick.db');
+
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-const talkedRecently = new Set();
 
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setPresence({ activity: { name: 'at Koala Cafe!' }, status: 'online' })
-  .then(console.log)
-  .catch(console.error);
-});
-
-client.on('ready', () => {
-    console.log('Koala Bot is ONLINE. No bugs found at this time.');
-
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    client.user.setActivity(' over Koala Cafe | =-help', { type: 'WATCHING' });
+    client.user.setStatus("online");
 });
 
 
 
-client.on('message', message => {    
-    if (message.mentions.members.firstKey() === '436253576537440256') {
-    message.channel.send(':zzz: :zzz: :sleeping: You woke me. How rude! :angry:')
-}
-    
-    
-    let sender = message.author;
-    
-    if (sender.bot) return;
-    if (message.channel.type === 'dm') {
-        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: https://discord.gg/FtYeKMu) to use the bot! **")
+          
+if (sender.bot) return;
+
+     if (message.channel.type === 'dm') {
+        var embederrordm = new Discord.RichEmbed()
+        .setTitle('Error 404:')
+        .setDescription('Our system can only detect things in a guild!')
+        .setImage('https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif')
+        .setFooter('Error 404: DM Message')
+        .setColor('#000000');
+        message.channel.send(embederrordm);
         return;
     }
     
-    if (message.mentions.members.firstKey() === '422393536420511752') {
-    message.channel.send(':zzz: :zzz: :sleeping: You woke me. How rude! :angry:')
-    }
+client.on("message", message => {
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  prefix = prefixMention.match(message.content) ? message.content.match(prefixMention)[0] + " " : prefix;
+    message.channel.send(`Hi there! How can I help? My prefix is 'KC'.`);
+}
+ 
+client.on('guildMemberAdd', member => {
+        var embedinfo = new Discord.RichEmbed()
+        .setFooter("© Koala Bot | Made by Aaron #1742")
+        .setColor(randomColor)
+        .setTitle(":wave: Welcome")
+        .addField("Rules","Please make sure to read the rules, they are located in #rules")
+        .addField("Verify","To access all channels in the server, please verify your Roblox account in #verify")
+        .setDescription("Welcome to the Koala Cafe server!")
+     member.sendEmbed(embedinfo);
+       console.log(`${member.user.username} has joined`);
+});
 
         let msg = message.content.toLowerCase();
         let args = message.content.slice(prefix.length).trim().split(" ");
         let cmd = args.shift().toLowerCase();
 
         if (!message.content.startsWith(prefix)) return;
-        
+
         try {
-            if (talkedRecently.has(message.author.id))
-        return;
+
             let commandFile = require(`./commands/${cmd}.js`);
             commandFile.run(Discord, client, message, args);
-            talkedRecently.add(message.author.id);
-setTimeout(() => {
-            talkedRecently.delete(message.author.id);
-}, 2500);
 
         } catch (e) {
 
@@ -71,19 +68,10 @@ setTimeout(() => {
         }
     })
 
-client.on('guildMemberAdd', member => {
-    var embedinfo = new Discord.RichEmbed()
-        .setFooter("© Koala Bot | Made by Aaron #1742")
-        .setColor(randomColor)
-        .setTitle(":wave: Welcome")
-        .addField("Rules","Please make sure to read the rules, they are located in #rules")
-        .addField("Verify","To access all channels in the server, please verify your Roblox account in #verify")
-        .setDescription("Welcome to the Koala Cafe server!")
-     member.sendEmbed(embedinfo);
-       console.log(`${member.user.username} has joined`);
-});
+
 
 ////////////////////////////////////////////////////////////
+
 
 
 
