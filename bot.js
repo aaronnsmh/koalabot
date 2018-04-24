491334252a4d4adcc94cafa4d7bad240b3f60b22
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 16:36:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,44 @@
+// Koala Bot
+// Version: 0.9 PRE-ALPHA / PRE-REALEASE
+// Discord Code: 
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = 'KC';
 const db = require('quick.db');
+const prefix = 'KC';
 
-var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
-
+   
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    client.user.setActivity(' over Koala Cafe | =-help', { type: 'WATCHING' });
+    client.user.setActivity('you | KChelp', { type: 'WATCHING' });
     client.user.setStatus("online");
 });
 
 
+client.on('message', message => {
 
-          
+    let sender = message.author;
 
-     if (message.channel.type === 'dm') {
-        var embederrordm = new Discord.RichEmbed()
-        .setTitle('Error 404:')
-        .setDescription('Our system can only detect things in a guild!')
-        .setImage('https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif')
-        .setFooter('Error 404: DM Message')
-        .setColor('#000000');
-        message.channel.send(embederrordm);
+    if (sender.bot) return;
+    if (message.channel.type === 'dm') {
+        message.channel.send("**Unfortunately we can only read things in the Koala Cafe server). The comamnd prefix is 'KC' **")
         return;
-    }
+}
     
-client.on("message", message => {
-  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-  prefix = prefixMention.match(message.content) ? message.content.match(prefixMention)[0] + " " : prefix;
-    message.channel.send(`Hi there! How can I help? My prefix is 'KC'.`);
-});
- 
+
+   
+   
+   if (message.mentions.members.firstKey() === '383658506060038144') {
+    message.channel.send(':zzz: :zzz: :sleeping: You woke me. How rude! :angry: My prefix is -')
+   }
+   
 client.on('guildMemberAdd', member => {
-        var embedinfo = new Discord.RichEmbed()
-        .setFooter("© Koala Bot | Made by Aaron #1742")
+        let name = client.user.username;
+    var embedinfo = new Discord.RichEmbed()
+        .setFooter("© " + name + " | Made by Aaron #1742")
         .setColor(randomColor)
         .setTitle(":wave: Welcome")
-        .addField("Rules","Please make sure to read the rules, they are located in #rules")
-        .addField("Verify","To access all channels in the server, please verify your Roblox account in #verify")
-        .setDescription("Welcome to the Koala Cafe server!")
+        .setDescription("Use -cmds to find out about the commands!")
      member.sendEmbed(embedinfo);
        console.log(`${member.user.username} has joined`);
 });
@@ -62,17 +60,10 @@ client.on('guildMemberAdd', member => {
 
         } finally {
 
-            console.log(`${message.author.username} ran the command: ${cmd} sucessfully!`);
+            console.log(`${message.author.username} ${message.author.id} ran the command: ${cmd} probably unsuccsesfully!`);
 
         }
-    
+    })
 
 
-
-////////////////////////////////////////////////////////////
-
-
-
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
