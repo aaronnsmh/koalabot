370ca5cebb7b3a9c9416fae667da01115721fe02
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 17:00:09 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,13 +2,11 @@ const { get } = require("snekfetch");
 
 
 exports.run = (Discord, client, message, args) => {
-try {
-			get('https://aws.random.cat/meow').then(res => {
-				const embed = new Discord.RichEmbed()
-				.setImage(res.body.file)
-				return msg.channel.send({embed});
-			});
-		} catch(err) {
-			return msg.channel.send(error.stack);
-}
+	const { body } = await superagent
+	   .get('aws.random.cat/meow');
+	   const embed = new Discord.RichEmbed()
+	   .setColor(0x954D23)
+	   .setTitle("Meow :cat:")
+	   .setImage(body.file)
+message.channel.send({embed})
 }
