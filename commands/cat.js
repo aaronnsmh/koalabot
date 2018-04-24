2b6698c3664a5c3da717bb1ca9ca77715a1ef471
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 17:21:27 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,6 +2,6 @@
 
 
 exports.run = (Discord, client, message, args) => {
-message.channel.send(`This command isn't quit purrfect yet and will be coming soon. Meow :cat: )
+message.channel.send(`This command isn't quit purrfect yet and will be coming soon. Meow :cat: `)
 
 }
