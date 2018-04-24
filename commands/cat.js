f910a203adfb58bb0879c33b820a19fc595c392f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 17:15:35 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,11 +2,12 @@
 
 
 exports.run = (Discord, client, message, args) => {
-	const { body } = require("superagent");
+	const { body } = await superagent
 	   .get('aws.random.cat/meow');
 	   const embed = new Discord.RichEmbed()
 	   .setColor(0x954D23)
 	   .setTitle("Meow :cat:")
 	   .setImage(body.file)
 message.channel.send({embed})
+
 }
