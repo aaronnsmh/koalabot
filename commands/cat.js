014a85e668a4f511b87fb9931421f71f5c00e8cc
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-25 18:24:16 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,11 +1,25 @@
 
-
-
 exports.run = (Discord, client, message, args) => {
- const embed = new Discord.RichEmbed()
+    
+    let name = client.user.username;
+    let thumbnail = client.user.avatarURL;
+
+    
+function doMagic8BallVoodoo() {
+   var rand = ['https://i.imgur.com/WQ5V8Rd.jpg', 'https://i.imgur.com/ZgHOitF.jpg', 'https://i.imgur.com/m3P3vwk.gif', 'https://i.imgur.com/WLMGXBh.gif', 'https://i.imgur.com/VEbyKim.jpg', 'https://i.imgur.com/VEbyKim.jpg'];
+
+    return rand[Math.floor(Math.random()*rand.length)];
+}
+    const author = message.author;
+		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-		   .setTitle("This command isn't quit purrfect yet and will be coming soon. Meow :cat:")
-       .setImage('https://media.discordapp.net/attachments/424892536851529729/438746567810482177/cats-master-header_vp.png')
+		   .setThumbnail(thumbnail)
+       .setAuthor(name,thumbnail)
+		   .setTitle("Meow :cat:")
+       .setImage(doMagic8BallVoodoo())
+		   .setFooter("Aww! Requested by " + author + " at ")
 		   .setTimestamp()
-message.channel.send({embed})
+		   message.channel.send({embed})
+    
+
 }
