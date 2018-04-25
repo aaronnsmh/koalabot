1be3456a9e6ad855334588e4c7a73a23777a37e0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-25 20:46:15 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -9,6 +9,22 @@ let allowedRole = message.guild.roles.find("name", "bot admin");
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
+       let reason = args.slice(0).join(" ");
+       let staffc = message.guild.channels.find("name", "logs")
+        
+        if (!staffc) 
+            return message.reply('I cannot find a logs channel');
+
+        if (reason.length < 1) 
+            return message.reply('You must supply something to say.');
+
+
+        const embed = new Discord.RichEmbed()
+            .setColor(0x8cff00)
+            .setTimestamp()
+            .setDescription(`**Action:** Say\n**Target:** ${user.tag}\n**User:** ${message.author.tag}\n**They said:** ${reason}`);
+        staffc.send({embed});
+
         
        } else {
          message.channel.send("wot u doin? this command is for kewl ppl only (If you think this is wrong, make sure you have the 'bot admin` discord role)")
