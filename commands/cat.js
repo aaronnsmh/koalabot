b1ce60fa94127a0c0adc186b20aa6a022cf64cb3
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-25 18:02:29 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ exports.run = (Discord, client, message, args) => {
  const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setTitle("This command isn't quit purrfect yet and will be coming soon. Meow :cat:")
-       .setImage('http://1.bp.blogspot.com/-0WagJ8BL5WU/U5UKkIPfJ6I/AAAAAAAAB1c/BOpklEXhEWY/s1600/Quasi.jpg')
+       .setImage('https://media.discordapp.net/attachments/424892536851529729/438746567810482177/cats-master-header_vp.png')
 		   .setTimestamp()
 message.channel.send({embed})
 }
