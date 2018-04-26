436991a109adfb43ce15ad160ac1edca045461cb
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-26 16:43:24 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -17,6 +17,7 @@ function doMagic8BallVoodoo() {
 		   .setThumbnail("https://pbs.twimg.com/profile_images/178167256/8ball.jpg.jpeg")
 		   .setTitle("Magic 8 ball :8ball:")
 		   .setDescription(`**${author.tag} Asked:**\n${question}`)
+		   .addBlankField(true)
 		   .addField("Result:",doMagic8BallVoodoo())
 		   .setFooter("Requested by " + message.author.tag + " at ")
 		   .setTimestamp()
