8526ce9077834970220f44475e5d0ca15c2148a0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-26 16:25:22 +0100

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,7 @@
 exports.run = (Discord, client, message, args) => {
-
+let messageauthortag = message.author.tag
+let question = args.slice(0).join(" ");
+let author = message.author
 function doMagic8BallVoodoo() {
     var rand = [':8ball: It is certain',':8ball: As I see it, yes',':8ball: It is decidedly so',':8ball: Without a doubt',':8ball: Yes definitely',':8ball: You may rely on it',':8ball: Most Likely',':8ball: Outlook good',':8ball: Yes',':8ball: Signs point to yes',':8ball: Reply hazy try again',':8ball: Ask again later',':8ball: Better not tell you now',':8ball: Cannot predict now',':8ball: Concentrate and ask again',':8ball: Do not count on it',':8ball: My reply is no',':8ball: My sources say no',':8ball: Outlook not so good',':8ball: Very doubtful'];
 
