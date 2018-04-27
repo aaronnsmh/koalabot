3e8c3c33aab1366f62dc64f6397d361e131bc0eb
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 18:36:55 +0100

    Update awards.js

--- commands/awards.js
+++ commands/awards.js
@@ -2,11 +2,9 @@ exports.run = (Discord, client, message, args) => {
 
 
        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("wot u doin? this command is for kewl ppl only (If you think this is wrong, make sure you have the administrator discord permission)")
-		   const color = args[0]
-				
-		   const text = args.slice(1).join(" ");
+		   
 		   const author = message.author;
-		   if (text.length < 1) return message.channel.send("Can not announce nothing");
+		   
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#e8a60d")
