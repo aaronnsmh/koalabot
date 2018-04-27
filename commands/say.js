d683f1314b3043beca6cc5eed0ec7746cc43cc80
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 08:28:56 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,7 +3,8 @@ exports.run = (Discord, client, message, args) => {
 let sender = message.author;
 let allowedRole = message.guild.roles.find("name", "bot admin");
 
-       if(message.member.roles.has(allowedRole.id)) {
+       if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("wot u doin? this command is for kewl ppl only (If you think this is wrong, make sure you have the administrator discord permission)")
+
         const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
@@ -26,9 +27,5 @@ let allowedRole = message.guild.roles.find("name", "bot admin");
         staffc.send({embed});
 
         
-       } else {
-         message.channel.send("wot u doin? this command is for kewl ppl only (If you think this is wrong, make sure you have the 'bot admin` discord role)")
-         message.delete()
-         .then(m=>m.delete(10000))
-       }
+        
 }
