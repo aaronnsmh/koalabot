0095620d9e88dbefe51cbaaa2c829f68162cf2a0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:11:09 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -1,5 +1,7 @@
-const api = "http://jsonplaceholder.typicode.com/posts";
+
 const snekfetch = require("snekfetch");
 module.exports.run = async (bot, message, arge) => {
+    let reason = args.slice(1).join(" ");
+    const api = "https://verify.eryn.io/api/user/${reason}";
     snekfetch.get(api).then(console.log);
 }
