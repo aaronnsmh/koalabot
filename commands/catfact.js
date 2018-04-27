a5f877435488ebd05f94c715c0543660be8e1d07
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 20:45:25 +0100

    Update catfact.js

--- commands/catfact.js
+++ commands/catfact.js
@@ -4,6 +4,6 @@ exports.run = (Discord, client, message, args) => {
 module.exports.run = async (bot, Discord, client, message, args) => {
     let {body} = await superagent
     .get(`https://catfact.ninja/fact`);
-    message.channel.send(body.fact)
+    message.reply(body.fact)
 }
 }
