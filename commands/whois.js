cdbdff22e7e895ebfde329fc47a53d75a113cf4f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 20:32:03 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -5,6 +5,6 @@ module.exports.run = async (bot, Discord, client, message, args) => {
     let address = 354349136998301706
     let {body} = await superagent
     let site = "https://verify.eryn.io/api/user/" + address
-    .get(`https://verify.eryn.io/api/user/354349136998301706);
+    .get(`https://verify.eryn.io/api/user/354349136998301706`);
     console.log(body.robloxUsername)
 }
