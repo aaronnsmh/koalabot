c4f0e2ef7416e7fb657c996299c728a94b9db56b
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 20:21:56 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -3,7 +3,6 @@ const superagent = require("superagent");
 module.exports.run = async (bot, Discord, client, message, args) => {
     let {body} = await superagent
     .get(`https://random.dog/woof.json`);
-    
-    message.channel.send(body.url);
+    console.log(body.url)
 }
 
