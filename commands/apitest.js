70951eacb9148829a695f28b2e430b0e7e7e394b
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:34:03 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, Discord, client, message, args) => {
     const api = "http://jsonplaceholder.typicode.com/posts";
     snekfetch.get(api).then(r => {
         let body = r.body;
-        let id = Number(args[0]);
+        let id = 1;
         let entry = body.find(post => post.id === id);
         message.channel.send(entry);
 });
