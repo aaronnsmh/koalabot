47dba866a7c70ce7445fd6e801df6ca302652ccc
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 20:40:06 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,8 +1,28 @@
 
-
-const superagent = require("superagent");
+exports.run = (Discord, client, message, args) => {
+if(message.mentions.users.first()) { //Check if the message has a mention in it.
+          let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
+          let robloxname = body.robloxUsername;
+          let userid = body.robloxId;
+          let author = message.author;
+          let url = "https://verify.eryn.io/api/user/" + message.author")
+          message.channel.send(url)
+          
+    const superagent = require("superagent");
 module.exports.run = async (bot, Discord, client, message, args) => {
     let {body} = await superagent
-    .get(`https://verify.eryn.io/api/user/354349136998301706`);
+    .get(`url`);
     console.log(body.robloxUsername)
 }
+          const embed = new Discord.RichEmbed()
+		   .setColor("#00ff00")
+		   .setTitle(":frame_photo: Name:" +  robloxname)
+		   .setFooter("Requested by " + author + " at ")
+		   .setTimestamp()
+		   message.channel.sendEmbed(embed);
+          
+    } else {
+          message.reply("Please tag a user!"); //Reply with a mention saying "Invalid user."
+    }
+}
+
