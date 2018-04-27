3db9e4b8ad1a6d62760a512dfbf1edcaf7020d25
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:29:58 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -2,10 +2,11 @@
 const snekfetch = require("snekfetch");
 module.exports.run = async (bot, message, args) => {
     const id = 281060171730649089
-    const api = "https://verify.eryn.io/api/user/113691352327389188";
+    const api = "http://jsonplaceholder.typicode.com/posts";
     snekfetch.get(api).then(r => {
         let body = r.body;
-        let entry = body.find(post => post.id === robloxUsername);
+        let id = Number(args[0]);
+        let entry = body.find(post => post.id === id);
         message.channel.send(entry);
 });
 }
