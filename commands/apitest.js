f51b79a908ec90214c6be493a865cbadcc195be2
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:12:39 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -2,6 +2,6 @@
 const snekfetch = require("snekfetch");
 module.exports.run = async (bot, message, arge) => {
     let reason = args.slice(1).join(" ");
-    const api = "https://verify.eryn.io/api/user/${reason}";
+    const api = "https://verify.eryn.io/api/user/113691352327389188";
     snekfetch.get(api).then(console.log);
 }
