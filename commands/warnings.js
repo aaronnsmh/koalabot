1710202d9a86f0c45e5308056c58d3d7a06c7e16
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:53:45 +0100

    Update warnings.js

--- commands/warnings.js
+++ commands/warnings.js
@@ -1,5 +1,5 @@
 exports.run = (Discord, client, message, args) => {
-message.chanel.send("Please search the following in #logs by clicking in the search box:")
-let user = message.mentions.users.first();
-message.channel.send("```in: logs Warning" + user.id + "```")
+    let user = message.mentions.users.first();
+    message.channel.send(`Please search the following in #logs by clicking in the search box:`);
+    message.channel.send('```in: logs Warning' + 'user.id' + '```')
 }
