a501e5ca031839558cd9cda80ef4991bf7cbad94
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 18:00:36 +0100

    Update warnings.js

--- commands/warnings.js
+++ commands/warnings.js
@@ -3,7 +3,6 @@ exports.run = (Discord, client, message, args) => {
     message.channel.send(`Please search the following in #logs by clicking in the search box:`);
     const embed = new Discord.RichEmbed()
             .setColor(0x8cff00)
-            .setTimestamp()
             .setDescription('in: logs Warning ' + user.tag);
     message.channel.send({embed});
 }
