f460e84ea62e210f67fa2a18264a558361e73187
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 20:29:59 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -2,7 +2,7 @@
 
 const superagent = require("superagent");
 module.exports.run = async (bot, Discord, client, message, args) => {
-    let address = message.author.id;
+    let address = 354349136998301706
     let {body} = await superagent
     let site = "https://verify.eryn.io/api/user/" + address
     .get(`https://verify.eryn.io/api/user/` + address);
