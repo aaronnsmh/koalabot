115a82742c3d151512ae2f31103c4a91884d637c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-27 17:45:46 +0100

    Update apitest.js

--- commands/apitest.js
+++ commands/apitest.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, Discord, client, message, args) => {
     const api = "https://verify.eryn.io/api/user/113691352327389188";
     snekfetch.get(api).then(r => {
         let body = r.body;
-        let id = robloxUsername
+        let id = robloxUsername;
+        let robloxUsername = robloxUsername;
         let entry = body.find(post => post.robloxUsername = id);
         console.log(entry);
 });
