2ea69eae19facc84abad23fa1145651c64464984
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-28 15:07:19 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,26 +1,21 @@
 
-
 const superagent = require("superagent");
 exports.run = async (Discord, client, message, args) => {
 	let baseurl = "https://verify.eryn.io/api/user/"
-	let baseurl2 = "https://www.roblox.com/bust-thumbnail/json?userId="
-	let endurl = "&height=180&width=180"
     	let reason1 = message.mentions.users.first();
 	let reason = reason1.id;
 	const url = baseurl + reason;
 	let {body} = await superagent
 	.get(url);
-	let id = body.robloxId;
-	const url2 = baseurl + id + endurl;
-	let {avatar} = await superagent
-	.get(url2)
 	let username = body.robloxUsername;
+	let id = body.robloxId;
+	let avatarurl = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userid=" + id;
           const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setTitle(":video_game: Get Info")
 	  	   .setDescription("**Roblox Name: **\n" + username + "\n**Roblox ID:**\n" + id)
 		   .setFooter("Requested by " + message.author.id + " at ")
 		   .setTimestamp()
-	  	   .setThumbnail(avatar.Url)
+	  	   .setImage(avatarurl)
 		   message.channel.sendEmbed(embed);
 }
