b9e80275e28a8a79ec0aa7a1dc1c8c51650e736e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-28 14:21:55 +0100

    Update whoisid.js

--- commands/whoisid.js
+++ commands/whoisid.js
@@ -3,12 +3,12 @@ const superagent = require("superagent");
 exports.run = async (Discord, client, message, args) => {
 	let baseurl = "https://verify.eryn.io/api/user/"
     	let reason1 = args.slice(0).join(" ");
-	let reason = reason1.id;
-	const url = baseurl + reason;
+	const url = baseurl + reason1;
 	let {body} = await superagent
 	.get(url);
 	let username = body.robloxUsername;
 	let id = body.robloxId;
+	message.channel.send(reason1)
           const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setTitle(":video_game: Get Info")
