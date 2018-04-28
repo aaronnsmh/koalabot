3afb8abba15b23c714e70988bc7dac609921085b
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-28 15:52:28 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -10,10 +10,11 @@ exports.run = async (Discord, client, message, args) => {
 	let username = body.robloxUsername;
 	let id = body.robloxId;
 	let avatarurl = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userid=" + id;
+	let groupurlbase = "https://www.roblox.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&groupid=937709&playerid=";
+	let groupurl = groupurlbase + id;
           const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-		   .setTitle(":video_game: Get Info")
-	  	   .setDescription("**Roblox Name: **\n" + username + "\n**Roblox ID:**\n" + id + "\n**Profile Link:** \n" + "https://www.roblox.com/users/" + id)
+	  	   .setDescription("**Roblox Name: **\n" + username + "\n**Roblox ID:**\n" + id + "\n**Profile Link:** \n" + "https://www.roblox.com/users/" + id + "\n**Rank In Group:**\n" + groupurl)
 		   .setFooter("Requested by " + message.author.id + " at ")
 		   .setTimestamp()
 	  	   .setImage(avatarurl)
