059ec8562f7c6bfc2a725c83ab700888234db6d8
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-29 08:56:26 +0100

    Create gif.js

---
+++ commands/gif.js
@@ -0,0 +1,10 @@
+const superagent = require("superagent");
+exports.run = async (Discord, client, message, args) => {
+    let search = args.slice(0).join(" ");
+    if (search.length < 1) 
+        return message.reply('You must supply something to search.');
+    let {body} = await superagent
+    .get(`http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s=` + search);
+    let url = body.url
+    message.reply(url)
+}
