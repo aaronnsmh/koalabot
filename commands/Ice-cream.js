67b8e58f0bc8657abc68ac22475417ea8bde3d75
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-29 22:49:30 +0100

    Create Ice-cream.js

---
+++ commands/Ice-cream.js
@@ -0,0 +1,6 @@
+
+
+exports.run = (Discord, client, message, args) => {
+
+message.channel.send('https://cdn.discordapp.com/attachments/334833519840985089/440267724636160020/BlackKidIceCream.jpg')
+}
