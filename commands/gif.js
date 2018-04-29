958ceb0c5054e8c1611ab8fb8903efcd3217f094
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-29 09:20:37 +0100

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -3,8 +3,8 @@ exports.run = async (Discord, client, message, args) => {
     let search = args.slice(0).join(" ");
     if (search.length < 1) 
         return message.reply('You must supply something to search.');
-    let {data} = await superagent
+    let {body} = await superagent
     .get(`http://api.giphy.com/v1/gifs/translate?api_key=dc6zaTOxFJmzC&s=` + search);
-    let url = data.data.url
-    message.reply(data.data.url)
+    let url = body.data.url
+    message.reply(body.data.url)
 }
