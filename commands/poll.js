075341ff486048623f71e632f69b1ac63aa341b7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-07 14:52:11 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -7,9 +7,9 @@ exports.run = async (Discord, client, message, args) => {
   var admin= new Discord.RichEmbed()
         .setTitle("Poll: " + "What game do you want to play?")
         .setDescription("React below with 1, 2 or 3!")
-        .addField("Vote 1:", "args[1]")
-        .addField("Vote 2:", "args[1]")
-        .addField("Vote 3:", "args[1]")
+        .addField("Vote 1:", args[1])
+        .addField("Vote 2:", args[1])
+        .addField("Vote 3:", args[1])
         .setFooter("Poll requested by: " + message.author.id + " at: ")
         .setTimestamp()
     message.channel.sendEmbed(admin);
