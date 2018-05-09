9791a513480141a1e2ab40742c996ee3d79f2ee2
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-09 19:49:05 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,9 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
+const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
+    db.fetch(guildPrefix_${message.guild.id}, args.join().trim()).then(i => {
 
+   
     message.reply("Here is the help you are looking for:");
     var embedcmds= new Discord.RichEmbed()
         .setFooter("© Koala Bot | Made by Aaron (Arron)")
@@ -11,7 +13,7 @@ exports.run = (Discord, client, message, args) => {
         .addField("What is koala cafe?", "Life.")
         .addField("Plss help me i need help verifying, please fam :cool: ", "Go to #verify and run !verify.")
         .addField("what the f**k is this bot", "This bot is a bot here for your entertainment, and for moderation comamnds, run KCcmds for more info!")
-        .addField("wot is the prefix?", "KC[CMD]")
+        .addField("wot is the prefix?", i + "[CMD]")
         .addField("More help coming soon!", "Because you need it.")
     message.channel.sendEmbed(embedcmds);
     message.delete(10000)
