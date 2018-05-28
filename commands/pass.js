7ada3f2cec1b7e9ed9f3ed08f2e0874436f4b2b0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-28 08:41:55 +0100

    Update pass.js

--- commands/pass.js
+++ commands/pass.js
@@ -10,35 +10,37 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 
 }).catch(() => {console.log("Failed to login.");});
 
-    var user = args[0];
-    var username = args[0];
-    var targetRank = 4;
-    if (username){
-        message.channel.send(`Checking ROBLOX for ${username}`)
-        roblox.getIdFromUsername(username)
-            .then(function(id){
-                roblox.getRankInGroup(groupid, id)
-                .then(function(rank){
-                    if(maximumRank <= rank){
-                    message.channel.send(`${id} is above rank limit.`)
-                    } else {
-                    roblox.setRank(groupId, user, targetRank)
-		    message.channel.send(`Passed user ${username}, rank changed from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
+    	var username = args[0]
+    	if (username){
+    		roblox.getIdFromUsername(username)
+			.then(function(id){
+				roblox.getRankInGroup(groupId, id)
+			        
+				.then(function(rank){
+					if(maximumRank <= rank){
+						message.channel.send(`${id} is rank ${rank} and not passable.`)
+					} else {
+						roblox.changeRank(groupId, id, 2)
+						
+						.then(function(roles){
+							message.channel.send(`Passed user ${username}, rank changed from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
 							const embed = new Discord.RichEmbed()
 							    .setColor(0x8cff00)
 							    .setTimestamp()
 							    .setDescription(`**Action:** Pass\n**Target:** ${username}\n**User:** ${message.author.tag}\n**Old Rank:** ${roles.oldRole.Name}\n**New Rank:** ${roles.newRole.Name}`);
 							staffc.send({embed});
-                    .catch(function(err){
-                        message.channel.send("Please check rank and username spelling.")
-                    });
-                }	
-						   
-						   
-						   
-						   
-						   
-						   
-							
-						
+						}).catch(function(err){
+							message.channel.send("Failed to pass.")
+						});
+					}
+				}).catch(function(err){
+					message.channel.send("Couldn't get them in the group.")
+				});
+			}).catch(function(err){ 
+				message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+			});
+    	} else {
+    		message.channel.send("Please enter a username.")
+    	}
+    	return;
 }
