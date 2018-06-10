31b96fca357229fcb3064998184a94eb58e488f7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-10 11:24:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,49 +19,32 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-
+client.on('message', async message => {
+let balance = await db.fetch(`balance_${message.author.id}`)
+if (balance === null) await db.set(`balance_${message.author.id}`, 0);
     let sender = message.author;
-
     if (sender.bot) return;
-    if (message.channel.type === 'dm') {
-        message.channel.send("**Unfortunately we can only read things in the Koala Cafe server). The comamnd prefix is 'KC' **")
-        return;
-    if(message.content == '.') {
-       let user = message.author;
-       let reason = args.slice(1).join(" ");
-       let staffc = message.guild.channels.find("name", "logs")
-       const embed = new Discord.RichEmbed()
-            .setColor(0x8cff00)
-            .setTimestamp()
-            .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
-        staffc.send({embed});
-        message.channel.send(`**${user.tag}** has been warned successfully for ${reason}`);
-        user.send(`You've been warned in Koala Cafe for **${reason}** by ${message.author.tag}! Tut tut...`);
-}
-}
-    
+
+
 
    
-   
-   if (message.mentions.members.firstKey() === '436253576537440256') {
-    message.channel.send(':zzz: :zzz: :sleeping: You woke me. How rude! :angry: My prefix is `KC')
-   }
-   
-   
 
-   if (message.mentions.members.firstKey() === '281060171730649089') {
-    message.channel.send('Thanks for tagging Aaron! He will get back to you soon!')
-   }
+
+
+   
+   var guildid = message.guild.id
+   let prefix = await db.fetch(`guildPrefix_${guildid}`)
+   if (prefix === null) prefix = "KC";
+ 
+        
 
         let msg = message.content.toLowerCase();
         let args = message.content.slice(prefix.length).trim().split(" ");
         let cmd = args.shift().toLowerCase();
 
         if (!message.content.startsWith(prefix)) return;
-        
-        try {
 
+        try {
             let commandFile = require(`./commands/${cmd}.js`);
             commandFile.run(Discord, client, message, args);
 
@@ -69,12 +52,10 @@ client.on('message', message => {
 
             console.log(e);
 
-        } finally {
-
-            console.log(`${message.author.username} ${message.author.id} ran the command: ${cmd} probably unsuccsesfully!`);
-
         }
-    })
+
+    
+})
 const { Client, Util } = require('discord.js');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
@@ -224,5 +205,42 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`🎶 Started playing **${song.title}**`);
 }
+/*client.on("message", async (message, guild) => {
+let member = message.author;
+let pass = await db.fetch(`ServerPass_451438570410999818`, { target: '.pass' });
+if(pass === 'true') console.log("password not enabled on this guild")
+else {
+message.author.send("Password Protection Is Enabled On " + guild + ", please enter the password.")
+let word = await db.fetch(`ServerPass_451438570410999818`, { target: '.word' });
+let role = await db.fetch(`ServerPass_451438570410999818`, { target: '.role' });
+let modRole = message.guild.roles.find("name", role);
+const collector = new Discord.MessageCollector(member, { time: 60000 });
+member.send("You have 60 seconds to enter the password.");
+collector.on('collect', async message => {
+    if (message.content == word) {
+        member.send("Correct password entered, you will now be roled in the server!")
+        await member.addRole(modRole);
+        member.send("You've succsesfully been roled, thanks for using iBot Password Protection")
 
+    }
+})
+}
+});
+
+client.on("guildCreate", async guild => {
+  const invite = await guild.channels.first().createInvite({
+    maxAge: 0
+  });
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  let channel = client.channels.get("449182354507038720")
+  let embed = new Discord.RichEmbed()
+  .setTitle("New Guild")
+  .setColor(randomColor)
+  .setFooter("iBot | New Guild")
+  .setTimestamp()
+  .setThumbnail('https://cdn.discordapp.com/attachments/379006942875746306/451309167127560193/circle.png')
+  .setDescription("iBot has joined a new guild called, " + guild.name + "! It is owned by " + guild.owner.username + ".")
+channel.send(embed)
+})*/
+          
 client.login(process.env.BOT_TOKEN);
