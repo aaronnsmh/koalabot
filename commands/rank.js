19d14479672cbbeac131e435ff7c0cfdbc18fd67
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-11 17:28:22 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,11 +1,11 @@
 var roblox = require('noblox.js')
 exports.run = async (Discord, client, message, args) => {
-	let name = args[2];
+	let name = args[0];
 	if (!name) 
 		return message.reply('Please enter a username.');
   roblox.getIdFromUsername(name)
   .then(function(id){
-				roblox.getRankInGroup(937709, id)
+	  roblox.getRankNameInGroup(937709, id)
 			        
 				.then(function(rank){
         message.channel.send(name+"'s rank: **"+rank+"**")
