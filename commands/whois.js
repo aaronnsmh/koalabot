c0e304be90e6d22feadbe7be1d571e8fa981c72a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-11 17:17:09 +0100

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -1,4 +1,4 @@
-
+var roblox = require('noblox.js')
 const superagent = require("superagent");
 exports.run = async (Discord, client, message, args) => {
 	let baseurl = "https://verify.eryn.io/api/user/"
@@ -14,11 +14,12 @@ exports.run = async (Discord, client, message, args) => {
 	if (id.length < 1) 
 		return message.reply('You are not verified yet! Please verify with KCverify.');
 	let avatarurl = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userid=" + id;
+	let rig = await roblox.getRankNameInGroup(937709, id)
 	let groupurlbase = "https://www.roblox.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRole&groupid=937709&playerid=";
 	let groupurl = groupurlbase + id;
           const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-	  	   .setDescription("**Roblox Name: **\n" + username + "\n**Roblox ID:**\n" + id + "\n**Profile Link:** \n" + "https://www.roblox.com/users/" + id + "\n**Rank In Group:**\n" + rank)
+	  	   .setDescription("**Roblox Name: **\n" + username + "\n**Roblox ID:**\n" + id + "\n**Profile Link:** \n" + "https://www.roblox.com/users/" + id + "\n**Rank In Group:**\n" + rig)
 		   .setFooter("Requested by " + message.author + " at ")
 		   .setTimestamp()
 	  	   .setThumbnail(avatarurl)
