d8e5288004f590384c254907de3716b6287478ad
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-11 17:25:37 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,8 +1,8 @@
 var roblox = require('noblox.js')
 exports.run = async (Discord, client, message, args) => {
 	let name = args[2];
-	if (name.length = 0) 
-		return message.reply('You are not verified yet! Please verify with KCverify.');
+	if (!name) 
+		return message.reply('Please enter a username.');
   roblox.getIdFromUsername(name)
   .then(function(id){
 				roblox.getRankInGroup(937709, id)
