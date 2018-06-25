11ba00dc6cae7142d77ea3da743c221a6bebbc04
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-25 17:50:38 +0100

    Update pass.js

--- commands/pass.js
+++ commands/pass.js
@@ -20,12 +20,7 @@ roblox.login({username: process.env.username, password: process.env.password}).t
 					if(maximumRank <= rank){
 						message.channel.send(`${id} is rank ${rank} and not passable.`)
 					} else {
-						var options = {
- 					      	 group: 937709,
-						 target: id,
-  						 name: 'Trainee'
-						}
-						roblox.setRank(options)
+						roblox.setRank(groupId, id, 4)
 						.then(function(roles){
 							message.channel.send(`Passed user ${username}, rank changed from ${roles.oldRole.Name} to ${roles.newRole.Name}`)
 							const embed = new Discord.RichEmbed()
