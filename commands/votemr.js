6ab07749f6464da669ad3ec328cb7b38399d6f93
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-27 17:03:51 +0100

    Update votemr.js

--- commands/votemr.js
+++ commands/votemr.js
@@ -6,14 +6,14 @@ roblox.getIdFromUsername(name)
   .then(function(id){
 	  let id2 = id
 	let avatarurl = "https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userid=" + id2;
-	let rig = await roblox.getRankNameInGroup(937709, id2)
+	let rig = roblox.getRankNameInGroup(937709, id2)
           const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
        .setTitle("Vote For" + args[0])
 	  	 .setDescription("**Profile Link:** \n" + "https://www.roblox.com/users/" + id + "\n**Rank In Group:**\n" + rig)
 		   .setFooter("React with ✅ or ⛔")
 	  	   .setThumbnail(avatarurl)
-     const pollTopic = await message.channel.sendEmbed(embed);
+     const pollTopic = message.channel.sendEmbed(embed);
     pollTopic.react(`✅`);
     pollTopic.react(`⛔`);	        
 });
