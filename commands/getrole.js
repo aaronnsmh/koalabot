de3e54864413c5a2cd61703193275bd8618ae78e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-14 08:05:01 +0100

    Update getrole.js

--- commands/getrole.js
+++ commands/getrole.js
@@ -1,14 +1,23 @@
-
-exports.run = (Discord, client, message, args) => {
-
-const author = message.author;
-		   const embed = new Discord.RichEmbed()
-		   .setColor("#00ff00")
-		   .setThumbnail('http://thewindowsclub.thewindowsclubco.netdna-cdn.com/wp-content/uploads/2016/04/windows-update-error.png')
-                   .setAuthor("Koala Verification")
-		   .setTitle("Uh Oh!")
-       		   .setDescription("It looks like you're trying to use a command that is still a W.I.P, to verify or getrole, please type something in #verify or say !verify.")
-		   .setFooter("Koala Bot | Made by Aaron#1742")
-		   .setTimestamp()
-		   message.channel.send({embed})
-}
+const request= require('request');
+	  const rbx= require('noblox');
+	  exports.run = async (Discord, client, message, args) => {
+		  request('https://verify.eryn.io/api/user/' + message.author.id, { json: true }, (err, res, body) => {
+              if (err || body.robloxUsername == null) {
+                    return message.channel.send("Hey there, :wave:! It looks like you're new around here. Please go to https://verify.eryn.io, verify and then try again!");              
+              } else { 
+                    message.member.setNickname(body.robloxUsername);
+                    message.member.addRole(message.guild.roles.find("name", "Verified"));
+                    message.reply("You've succesfully been verified!");
+					rbx.getRankNameInGroup(937709,body.robloxId).then(data => {
+						rbx.getRankInGroup(937709,body.robloxId).then(data1 => {
+							MessageUser.addRole(Verifyaa)
+							let RankForGroup = message.guild.roles.find("name",data);
+							await MessageUser.addRole(RankForGroup)
+							message.reply('Your roles have been updated, have a great day!)
+					})
+					})
+                }
+        });
+	
+	
+  }
