57043ba68cb4152f63c93d496a2cd8c407037aa4
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-18 16:24:32 +0100

    Update getrole.js

--- commands/getrole.js
+++ commands/getrole.js
@@ -3,15 +3,21 @@ const request= require('request');
 	  exports.run = async (Discord, client, message, args) => {
 		  request('https://verify.eryn.io/api/user/' + message.author.id, { json: true }, (err, res, body) => {
               if (err || body.robloxUsername == null) {
-                    return message.channel.send("Hey there, :wave:! It looks like you're new around here. Please go to https://verify.eryn.io, verify and then try again!");              
+                    return message.channel.send("Hi there, welcome to the Koala Cafe Discord. Please go to https://verify.eryn.io, verify your roblox account and then run this command again.");              
               } else { 
-                    message.member.setNickname(body.robloxUsername);
-                    message.member.addRole(message.guild.roles.find("name", "Verified"));
+                    message.member.setNickname(body.robloxUsername).catch(function(err){ 
+				message.channel.send(`I am unable to update your nickname.`)
+			});
+                    message.member.addRole(message.guild.roles.find("name", "Verified")).catch(function(err){ 
+				message.channel.send(`I was unable to give you the verified role.`)
+			});
 					rbx.getRankNameInGroup(937709,body.robloxId).then(data => {
 						rbx.getRankInGroup(937709,body.robloxId).then(data1 => {
 							let RankForGroup = message.guild.roles.find("name",data);
-							message.member.addRole(RankForGroup)
-							message.reply('Your roles have been updated, have a great day!')
+							message.member.addRole(RankForGroup).catch(function(err){ 
+				message.channel.send(`I was unable to update your role.`)
+			});
+							message.reply('Your account has been updated, have a great day.!')
 					})
 					})
                 }
