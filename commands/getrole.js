5df8681a336b65ff39be19e25503e5eda68749f0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-18 20:01:27 +0100

    Update getrole.js

--- commands/getrole.js
+++ commands/getrole.js
@@ -6,18 +6,29 @@ const request= require('request');
                     return message.channel.send("Hi there, welcome to the Koala Cafe Discord. Please go to https://verify.eryn.io, verify your roblox account and then run this command again.");              
               } else { 
                     message.member.setNickname(body.robloxUsername).catch(function(err){ 
-				message.channel.send(`I am unable to update your nickname.`)
+				message.channel.send(`Error: I was not able to update your discord nickname.`)
+			    	const newname = body.robloxUsername;
 			});
                     message.member.addRole(message.guild.roles.find("name", "Verified")).catch(function(err){ 
-				message.channel.send(`I was unable to give you the verified role.`)
+				message.channel.send(`Error: I was not able to give you the verified role.`)
 			});
 					rbx.getRankNameInGroup(937709,body.robloxId).then(data => {
 						rbx.getRankInGroup(937709,body.robloxId).then(data1 => {
 							let RankForGroup = message.guild.roles.find("name",data);
 							message.member.addRole(RankForGroup).catch(function(err){ 
-				message.channel.send(`I was unable to update your role.`)
+				message.channel.send(`Error: I was not able to update your discord role.`)
 			});
-							message.reply('Your account has been updated, have a great day.!')
+	
+							const embed = new Discord.RichEmbed()
+							    .setColor(0x8cff00)
+							    .setTimestamp()
+							    .setDescription(`${message.author.tag}, your Discord annount has been updated with the following infomation.`)
+							    .setTitle('Roblox Verification')
+							    .addField('Username',body.robloxUsername,true)
+							    .addField('ID',body.robloxId,true)
+							    .addField('Rank',RankForGroup,true)
+							    .setThumbnail("https://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&userid=" + body.robloxId")
+							message.channel.send({embed});
 					})
 					})
                 }
