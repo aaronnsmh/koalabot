28e9aa54f8a068968d21e6add38ddc8640052048
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-19 18:53:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,6 +85,16 @@ var randomchannelid = guild.channels.first().id;
 
 
 client.on('message', async message => {
+	if (message.content == "Alexa, play despacito.") {
+    if (message.member.voiceChannel) {
+        message.channel.send('Now you will feel the experience of Julio...')
+        const connection = await message.member.voiceChannel.join();
+        const voiceChannel = message.member.voiceChannel;
+        const dispatcher = connection.playStream(`https://cdn.glitch.com/355f66bb-1179-4f4c-8c1c-7d6c486e5e4e%2FLuis%20Fonsi%20-%20Despacito%20ft.%20Daddy%20Yankee%20%5BMpgun.com%5D.mp3`);
+        dispatcher.on('end', () => voiceChannel.leave());
+    } else {
+        message.reply('You need to join a voice channel first!');
+    }}
 if (message.channel.id === '469239109882216448') {
 	if (message.author.bot) {
          message.delete(10000)
