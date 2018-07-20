04f6e151427ebfd9b6f02b4b7be7a86fd4707ca7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-20 20:41:27 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,7 @@ exports.run = (Discord, client, message, args) => {
        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You need the ADMINISTRATOR permission to run this comamnd!")
 		   const color = args[0]
 		   let title = args[0];
-		   const text = args.slice(1).join(" ");
+		   const text = args.slice(0).join(" ");
 		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
