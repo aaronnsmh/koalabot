41a5cc9a1e909efdb83be4fadbc171dbb2c5004c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-07-24 08:57:03 +0100

    Create hannounce.js

---
+++ commands/hannounce.js
@@ -0,0 +1,22 @@
+exports.run = (Discord, client, message, args) => {
+
+      let allowedRole = message.guild.roles.find("name", "Announce Permissions");
+if(!message.member.roles.has(allowedRole.id)) return message.channel.send("You have invalid permissions!")
+		   const color = args[0]
+		   let title = args[0];
+		   const text = args.slice(0).join(" ");
+		   const author = message.author;
+		   if (text.length < 1) return message.channel.send("Can not announce nothing");
+		   //const colour = args.slice(2).join("");
+		   const embed = new Discord.RichEmbed()
+		   .setColor("#00ff00")
+		   .setThumbnail(client.user.avatarURL)
+		   .setTitle(":mega: Announcement: ")
+		   .setDescription("**Announced by: " + message.author + "**\n\n" + text + "\n")
+		   .setFooter("An announcment made at ")
+		   .setTimestamp()
+		   message.delete(10)
+		   message.channel.send("@here")
+		   message.channel.send({embed})
+	   
+}
