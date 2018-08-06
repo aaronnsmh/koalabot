f8a159343a4bc2a8c06884b96880885e8c407766
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-08-06 14:14:00 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,6 @@
 exports.run = (Discord, client, message, args) => {
     let reason = args.slice(1).join(" ");
     let user = message.mentions.users.first();
-    let staffc = message.guild.channels.find("name", "logs")
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if (!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
@@ -12,7 +11,7 @@ exports.run = (Discord, client, message, args) => {
             .setColor(0x8cff00)
             .setTimestamp()
             .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
-        staffc.send({embed});
+      
     message.channel.send(`**${user.tag}** has been kicked successfully for ${reason}`);
     user.send(`You've been warned in Koala Cafe for **${reason}** by ${message.author.tag}! Tut tut...`);
     message.guild.member(kUser).kick(kReason);
